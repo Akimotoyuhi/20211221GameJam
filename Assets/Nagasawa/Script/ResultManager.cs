11f07a0ae445c86e86f241d1769582b8aa8a610e
NagasawaYusei <curga3013@gmail.com>
2021-12-21 19:49:08 +0900

    途中

---
+++ Assets/Nagasawa/Script/ResultManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] Scenemanager _sceneManager;
+    [SerializeField] Image _image;
+    [SerializeField] 
+    void Start()
+    {
+        _image.color = Color.black;
+        _sceneManager.StartFadeIn();
+    }
+}
---
+++ Assets/Nagasawa/Script/StartScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScene : MonoBehaviour
+{
+    [SerializeField] Scenemanager _sceneManager;
+    [SerializeField] Image _image;
+    void Start()
+    {
+        _image.color = Color.black;
+        _sceneManager.StartFadeIn();
+    }
+}
