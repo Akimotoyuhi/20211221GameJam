67958d8fd743a3d1eca80fdd7b4db47b70af35a6
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2021-12-21 18:11:22 +0900

    雪を降らせるパーティクルの作成

--- Assets/Kitazume/Script/MoveForwardItem.cs
+++ Assets/Kitazume/Script/MoveForwardItem.cs
@@ -12,13 +12,7 @@ public class MoveForwardItem : MonoBehaviour
             return speed;
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * speed);
