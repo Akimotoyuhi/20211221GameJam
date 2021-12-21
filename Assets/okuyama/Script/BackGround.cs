1d3289c999da2c30ea4f5211742b8d97538b9235
Nana_77 <nana77amery@gmail.com>
2021-12-21 12:44:33 +0900

    背景の移動

---
+++ Assets/okuyama/Script/BackGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    /// <summary>スタート位置</summary>
+    private Vector2 startPos;
+    /// <summary>背景幅</summary>
+    private float repeatWidth;
+    /// <summary>背景のスピード</summary>
+    [SerializeField] float speed = 10;
+
+    void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider2D>().size.x/2;
+    }
+
+    /// <summary>背景の繰り返し</summary>
+    void Update()
+    {
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+        transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
+    }
+}
