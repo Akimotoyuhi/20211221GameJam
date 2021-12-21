8a7a66a11bf49c6d7a57e749200791785fd503f5
uen0_ <sarasa4869@zoho.com>
2021-12-21 13:54:27 +0900

    ScoreManagerをDistanceScoreManagerに変更、内容変更

---
+++ Assets/ueno/Sprict/DistanceScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceScoreManager : MonoBehaviour
+{
+    [SerializeField] Text _distanseText;
+    [SerializeField] Text _enemyText;
+    public int _distance = 0;
+    public int _enemy = 0;
+    //GameManager gameManager = default;
+    bool live;
+
+
+    void Start()
+    {
+        //gameManager = GetComponent<GameManager>();
+        live = true;
+    }
+
+    void Update()
+    {
+        _distanseText.text = "走った距離" + _distance.ToString("D8") + "M";
+        _enemyText.text = "ブラックサンタ" + _enemy.ToString("D4") + "体";
+
+        if (!live)
+        {
+            Debug.Log("シーン切替");
+            //gameManager.ChangeResultScene();
+        }
+    }
+}
