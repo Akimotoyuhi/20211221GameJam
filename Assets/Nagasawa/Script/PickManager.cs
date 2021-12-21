936aeb26296a11325293a4e0bc54905cd84ef86f
NagasawaYusei <curga3013@gmail.com>
2021-12-21 11:57:47 +0900

    Pick CreateStart

---
+++ Assets/Nagasawa/Script/PickManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PickManager : MonoBehaviour
+{
+    [SerializeField] EventSystem _eventSystem;
+    [SerializeField] Text[] _texts;
+    GameObject _selectObj;
+
+    void Start()
+    {
+        
+    }
+}
