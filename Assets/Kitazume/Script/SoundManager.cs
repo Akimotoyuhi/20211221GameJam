4964b01c6a720c87a071bebfcd8c7da764ac80c3
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2021-12-22 14:08:02 +0900

    AudioManager設置

---
+++ Assets/Kitazume/Script/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] AudioClip Audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
