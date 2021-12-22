cd64b70af54135baf2b344564fc8f018831348ef
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2021-12-22 15:48:00 +0900

    SoundManager完成

--- Assets/Kitazume/Script/SoundManager.cs
+++ Assets/Kitazume/Script/SoundManager.cs
@@ -1,20 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
+
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] AudioClip Audio;
+    private AudioSource audioSouse;
+    [SerializeField] AudioClip[] BGM;
+    [SerializeField] AudioClip[] SE;
+
+    public static SoundManager Instance { get; private set; }
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<AudioSource>();
+        audioSouse = gameObject.AddComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Instance = this;
     }
+    public void Play(AudioType Type, int ID)
+    {
+        switch (Type)
+        {
+            case AudioType.BGM:
+                audioSouse.clip = BGM[ID];
+                audioSouse.Play();
+                break;
+
+            case AudioType.SE:
+                audioSouse.PlayOneShot(SE[ID]);
+                break;
+        }
+
+       
+    }
+    // Update is called once per frame
+
+}
+
+
+public enum AudioType
+{
+    BGM,
+    SE,
 }
---
+++ Assets/Kitazume/Script/SoundTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+    [SerializeField] GameObject SoundMG;
+    [SerializeField] AudioType type;
+    [SerializeField] int ID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void OnClick()
+    {
+        SoundMG.GetComponent<SoundManager>().Play(type,ID);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
