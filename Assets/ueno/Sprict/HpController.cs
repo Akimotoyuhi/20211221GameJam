c4458acd0a56c3d68982a46083d5b83c16b498b1
uen0_ <sarasa4869@zoho.com>
2021-12-22 11:08:11 +0900

    GameSceneにUIを追加

--- Assets/ueno/Sprict/HpController.cs
+++ Assets/ueno/Sprict/HpController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class Hpcontroller : MonoBehaviour
+public class HpController : MonoBehaviour
 {
     /// <summary> Playerの体力 </summary>
-    [SerializeField] float maxHp = 5f;
+    [SerializeField] float _maxHp = 5f;
+    /// <summary>テキスト</summary>
+    [SerializeField] Text _hpText = default;
+
     Slider hpSlider;
-    // int Hp;
+
+    public int _hp = 0;
     void Start()
     {
         hpSlider = GetComponent<Slider>();
         //スライダーの最大値の設定
-        hpSlider.maxValue = maxHp;
+        hpSlider.maxValue = _maxHp;
+    }
+    
+    void Update()
+    {
+        _hpText.text = _hp.ToString();
     }
 
     /// <summary>Hpをスライダーに表示させるメソッド</summary>
